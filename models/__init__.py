a31955b3948b12ace6732b83b28f56306861a04d
Gaming <Gaming@LAPTOP-4194GSMC>
2022-12-10 07:16:22 -0800

    .

--- models/__init__.py
+++ models/__init__.py
@@ -13,8 +13,8 @@ from . import external_missions
 from . import Leave
 from . import x_secondment
 from . import AllowancesAndDedecutions
-#from . import hr_contract
-# from . import JudgePromotion
-# from . import TrainingAbsenceReq
-# from . import TrainingSchedule
-# from . import Departments
+from . import hr_contract
+from . import JudgePromotion
+from . import TrainingAbsenceReq
+from . import TrainingSchedule
+from . import Departments
